8dfc7faace13395f0cfdd483dfb467889b9cba7a
Oliver Runge <oliver.runge@gmail.com>
2025-02-09 09:34:09 +0100

    Add algorithm L implementation

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+type Function = u32;
+
+const INFINITY: u32 = 0xffffffff;
+
+fn find_normal_lengths(n: u32, target_functions: &Vec<Function>) -> Vec<u32> {
+    // L1. Initialize.
+    let mut lengths: Vec<u32> = vec![INFINITY; 2u32.pow(2u32.pow(n) - 1) as usize];
+    let mut lists: Vec<Vec<Function>> = vec![vec![]; 1];
+    let mut stats: Vec<u32> = vec![0; 1];
+    lengths[0] = 0;
+    stats[0] += 1;
+    let tautology = 2u32.pow(2u32.pow(n)) - 1;
+    for k in 1..=n {
+        let slice = 2u32.pow(2u32.pow(n - k)) + 1;
+        let f = tautology / slice;
+        lengths[f as usize] = 0;
+        lists[0].push(f);
+        stats[0] += 1;
+    }
+    // initialize c, the number of functions where L(f) = infinity
+    let mut c = 2u32.pow(2u32.pow(n) - 1) - n - 1;
+
+    // L2. Loop over r = 1, 2, ...
+    for r in 1.. {
+        while lists.len() <= r as usize {
+            lists.push(vec![]);
+        }
+        while stats.len() <= r as usize {
+            stats.push(0);
+        }
+        // L3. Loop over j = 0, 1, ..., r - 1 k = r - 1 - j while j <= k
+        for j in 0..=r - 1 {
+            let k = r - 1 - j;
+            if k < j {
+                break;
+            }
+            // L4. Loop over all functions g and h in lists j and k
+            for gi in 0..lists[j as usize].len() {
+                for hi in 0..lists[k as usize].len() {
+                    let g = lists[j as usize][gi];
+                    let h = lists[k as usize][hi];
+                    // L5. Loop over all new functions f
+                    for rf in [g & h, (!g) & h, g & (!h), g | h, g ^ h] {
+                        let f = rf & tautology;
+                        // L6. Add f if it is new
+                        if lengths[f as usize] == INFINITY {
+                            lengths[f as usize] = r;
+                            lists[r as usize].push(f);
+                            stats[r as usize] += 1;
+                            c -= 1;
+                            if c == 0 {
+                                for fi in target_functions {
+                                    let f = fi & tautology;
+                                    println!("{:016b}: {}", f, lengths[f as usize]);
+                                }
+                                return stats;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    stats
+}
+
+fn main() {
+    let target_functions: Vec<Function> = vec![
+        !0b1011011111100011,
+        !0b1111100111100100,
+        !0b1101111111110100,
+        !0b1011011011011110,
+        !0b1010001010111111,
+        !0b1000111111110011,
+        0b0011111011111111,
+    ];
+    let stats = find_normal_lengths(4, &target_functions);
+    println!("{:?}", stats);
+}
