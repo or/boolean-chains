c70022e4f21da9a24020bd57ce70467cbe259978
Oliver Runge <oliver.runge@gmail.com>
2025-02-13 00:15:53 +0100

    Make bit_set a static array in the C++ version
    This reduces the time to 2.0s.

--- src/bit_set_fast.h
+++ src/bit_set_fast.h
@@ -5,16 +5,20 @@
 using std::min;
 using std::vector;
 
+const size_t SIZE = 30;
+
 class BitSet {
 private:
-  vector<uint32_t> bit_set;
+  uint32_t bit_set[SIZE];
 
 public:
-  BitSet() : bit_set(vector<uint32_t>()) {}
-  BitSet(const BitSet &other) : bit_set(other.bit_set) {}
+  BitSet() { std::memset(bit_set, 0, sizeof(bit_set)); }
+  BitSet(const BitSet &other) {
+    std::memcpy(bit_set, other.bit_set, sizeof(bit_set));
+  }
 
   bool is_disjoint(const BitSet &other) const {
-    for (size_t i = 0; i < min(bit_set.size(), other.bit_set.size()); ++i) {
+    for (size_t i = 0; i < SIZE; ++i) {
       if (bit_set[i] & other.bit_set[i]) {
         return false;
       }
@@ -25,7 +29,7 @@ public:
   bool get(uint32_t bit) const {
     uint32_t index = bit >> 5;
     uint32_t bit_index = bit & 0b11111;
-    if (bit_set.size() <= index) {
+    if (SIZE <= index) {
       return false;
     }
     return bit_set[index] & (1 << bit_index);
@@ -34,29 +38,18 @@ public:
   void insert(uint32_t bit) {
     uint32_t index = bit >> 5;
     uint32_t bit_index = bit & 0b11111;
-    while (bit_set.size() <= index) {
-      bit_set.push_back(0);
-    }
     bit_set[index] |= (1 << bit_index);
   }
 
   void add(const BitSet &other) {
-    while (bit_set.size() < other.bit_set.size()) {
-      bit_set.push_back(0);
-    }
-    for (size_t i = 0; i < std::min(bit_set.size(), other.bit_set.size());
-         ++i) {
+    for (size_t i = 0; i < SIZE; ++i) {
       bit_set[i] |= other.bit_set[i];
     }
   }
 
   void intersect(const BitSet &other) {
-    for (size_t i = 0; i < bit_set.size(); ++i) {
-      if (i < other.bit_set.size()) {
-        bit_set[i] &= other.bit_set[i];
-      } else {
-        bit_set[i] = 0;
-      }
+    for (size_t i = 0; i < SIZE; ++i) {
+      bit_set[i] &= other.bit_set[i];
     }
   }
 };
