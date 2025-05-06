73997e5937090201fed9b54e31755d33edcff332
Oliver Runge <oliver.runge@gmail.com>
2025-05-06 13:41:51 +0200

    Add multi command capabilities to main.sh

--- boinc-central/main.sh
+++ boinc-central/main.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-(time $*) 2>&1 | tee output
+DELIMITER="##"
+
+JOINED_ARGS=""
+for arg in "$@"; do
+  if [ -z "$JOINED_ARGS" ]; then
+    JOINED_ARGS="$arg"
+  else
+    JOINED_ARGS="$JOINED_ARGS $arg"
+  fi
+done
+
+IFS="$DELIMITER" read -ra COMMANDS <<< "$JOINED_ARGS"
+for cmd in "${COMMANDS[@]}"; do
+  cmd=$(echo "$cmd" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  if [ -n "$cmd" ]; then
+    echo "Running command: $cmd" | tee -a output
+    echo "----------------------------------------" | tee -a output
+    (time $cmd) 2>&1 | tee -a output
+    echo "----------------------------------------" | tee -a output
+    echo "" | tee -a output
+  fi
+done
