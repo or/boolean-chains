bddf5f7cec87c94e6a9c7193967f6cabdcf11443
Oliver Runge <oliver.runge@gmail.com>
2025-05-24 15:52:16 +0200

    Remove special leaf-processing
    Also set to N=12, L=18.

--- src/full-search.cpp
+++ src/full-search.cpp
@@ -31,9 +31,9 @@
 #define CAPTURE_STATS_CALL(chain_size)
 #endif
 
-constexpr uint32_t N = 16;
+constexpr uint32_t N = 12;
 constexpr uint32_t SIZE = 1 << (N - 1);
-constexpr uint32_t MAX_LENGTH = 22;
+constexpr uint32_t MAX_LENGTH = 18;
 constexpr uint32_t TAUTOLOGY = (1 << N) - 1;
 constexpr uint32_t TARGET_1 =
     ((~(uint32_t)0b1011011111100011) >> (16 - N)) & TAUTOLOGY;
@@ -334,68 +334,15 @@ int main(int argc, char *argv[]) {
   }
 
   while (true) {
-    if (__builtin_expect(chain_size + num_unfulfilled_targets == MAX_LENGTH,
-                         1)) {
-      size_t tmp_chain_size = chain_size;
-      size_t generated_chain_size = chain_size - 1;
-      uint32_t tmp_num_unfulfilled_targets = num_unfulfilled_targets;
-      size_t j = choices[chain_size] + 1;
+  next:
+    GENERATE_NEW_EXPRESSIONS(chain_size, ADD_EXPRESSION)
 
-    next:
-      while (__builtin_expect(tmp_chain_size < MAX_LENGTH, 1)) {
-        GENERATE_NEW_EXPRESSIONS(tmp_chain_size, ADD_EXPRESSION_TARGET)
-        generated_chain_size = tmp_chain_size;
-
-        // CAPTURE_STATS_CALL(tmp_chain_size)
-
-        for (; j < expressions_size[tmp_chain_size]; ++j) {
-          total_chains++;
-          if (__builtin_expect((total_chains & 0xffffffff) == 0, 0)) {
-            PRINT_PROGRESS(tmp_chain_size, (uint32_t)j)
-          }
-
-          if (__builtin_expect(target_lookup[expressions[j]], 0)) {
-            chain[tmp_chain_size] = expressions[j];
-            tmp_num_unfulfilled_targets--;
-            tmp_chain_size++;
-            if (__builtin_expect(!tmp_num_unfulfilled_targets, 0)) {
-              print_chain(chain, target_lookup, tmp_chain_size);
-              goto leafs_done;
-            }
-            j++;
-            goto next;
-          }
-        }
-        break;
-      }
-
-    leafs_done:
-      for (size_t i = expressions_size[chain_size - 1];
-           i < expressions_size[generated_chain_size]; i++) {
-        seen[expressions[i]] = 1;
-      }
-
-      chain_size--;
-      num_unfulfilled_targets += target_lookup[chain[chain_size]];
-      // if it was a target function, then we can skip all other choices at this
-      // length, because the target function would now be in seen and prevent
-      // any successful chain from here on; this massively reduces the search
-      // space to about 50% the trick here is to simply add a large number to
-      // the choices at that level if target_lookup is 1, this avoids branching
-      choices[chain_size] += target_lookup[chain[chain_size]] << 16;
-      if (__builtin_expect(chain_size < stop_chain_size, 0)) {
-        return 0;
-      }
-    } else {
-      GENERATE_NEW_EXPRESSIONS(chain_size, ADD_EXPRESSION)
-
-      CAPTURE_STATS_CALL(chain_size)
-    }
+    CAPTURE_STATS_CALL(chain_size)
 
   restore_progress:
     while (true) {
       choices[chain_size]++;
-      if (choices[chain_size] < expressions_size[chain_size]) {
+      while (choices[chain_size] < expressions_size[chain_size]) {
         chain[chain_size] = expressions[choices[chain_size]];
 
 #if PLAN_MODE
@@ -416,6 +363,12 @@ int main(int argc, char *argv[]) {
         }
 
         num_unfulfilled_targets -= target_lookup[chain[chain_size]];
+        if (chain_size + num_unfulfilled_targets >= MAX_LENGTH) {
+          // no need to do this, as it must have been 0 to end up in this path
+          // num_unfulfilled_targets += target_lookup[chain[chain_size]];
+          choices[chain_size]++;
+          continue;
+        }
 
         if (__builtin_expect(!num_unfulfilled_targets, 0)) {
           print_chain(chain, target_lookup, chain_size + 1);
@@ -431,7 +384,7 @@ int main(int argc, char *argv[]) {
 
         chain_size++;
         choices[chain_size] = choices[chain_size - 1];
-        break;
+        goto next;
       }
 
     done:
